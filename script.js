dcb207e90481850bfb3a6deb7df09d197a5abca7
skikh <103261396+skikh@users.noreply.github.com>
2022-04-08 23:43:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    $("#btn").click(
+		function(){
+			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+			return false; 
+		}
+	);
+});
+ 
+function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+        url:"https://60376bfd5435040017722533.mockapi.io/formRej",//url страницы (action_ajax_form.php)
+        type: "POST", //метод отправки
+        dataType: "html", //формат данных
+        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        success: function(response) { //Данные отправлены успешно
+        	result = $.parseJSON(response);
+        	$('#form').html('Имя: '+result.name+'<br>Телефон: '+result.phone+'Email: '+result.email);
+    	},
+    	error: function(response) { // Данные не отправлены
+            $('#result_form').html('Ошибка. Данные не отправлены.');
+    	}
+ 	});
+}
